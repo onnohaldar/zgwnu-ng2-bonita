3694af40c034e99b36f1774ee077f7148a4f0351
Onno Haldar <onno@zgw.nu>
2017-03-29 11:20:05 +0200

    refactor

---
+++ index.ts
@@ -0,0 +1,53 @@
+// Bonita Rest Api Generic
+export { BonitaConfigService } from './bonita-rest-api/bonita-config.service'
+export { BonitaDataMappingInterface } from './bonita-rest-api/bonita-data-mapping.interface'
+export { BonitaResponse } from './bonita-rest-api/bonita-response'
+export { BonitaErrorResponse } from './bonita-rest-api/bonita-error-response'
+export { BonitaSearchParms } from './bonita-rest-api/bonita-search-parms'
+export { BonitaUtils } from './bonita-rest-api/bonita-utils'
+
+// Bonita Rest Api Authentication
+export { BonitaAuthenticationService } from './bonita-authentication/bonita-authentication.service'
+export { BonitaCredentials } from './bonita-authentication/bonita-credentials'
+export { BonitaSession } from './bonita-authentication/bonita-session'
+
+// Bonita Rest Api BPM Activity and Task
+export { BonitaBpmActivityService } from './bonita-bpm-activity-task/bonita-bpm-activity.service'
+export { BonitaBpmTaskService } from './bonita-bpm-activity-task/bonita-bpm-task.service'
+export { BonitaBpmHumanTaskService } from './bonita-bpm-activity-task/bonita-bpm-human-task.service'
+export { BonitaBpmUserTaskService } from './bonita-bpm-activity-task/bonita-bpm-user-task.service'
+export { BonitaActivity } from './bonita-bpm-activity-task/bonita-activity'
+export { BonitaHumanTask } from './bonita-bpm-activity-task/bonita-human-task'
+export { BonitaTask } from './bonita-bpm-activity-task/bonita-task'
+export { BonitaUserTask } from './bonita-bpm-activity-task/bonita-user-task'
+
+// Bonita Rest Api BPM Case
+export { BonitaBpmCaseService } from './bonita-bpm-case/bonita-bpm-case.service'
+export { BonitaCase } from './bonita-bpm-case/bonita-case'
+
+// Bonita Rest Api BPM Case Document
+export { BonitaBpmCaseDocumentService } from './bonita-bpm-case-document/bonita-bpm-case-document.service'
+export { BonitaDocument } from './bonita-bpm-case-document/bonita-document'
+export { BonitaDocumentCreateInput } from './bonita-bpm-case-document/bonita-document-create-input'
+export { BonitaDocumentUpdateInput } from './bonita-bpm-case-document/bonita-document-update-input'
+
+// Bonita Rest Api BPM Data
+export { BonitaBpmDataService } from './bonita-bpm-data/bonita-bpm-data.service'
+export { BonitaCaseVariable } from './bonita-bpm-data/bonita-case-variable'
+
+// Bonita Rest Api BPM Process
+export { BonitaBpmProcessService } from './bonita-bpm-process/bonita-bpm-process.service'
+export { BonitaCreateCaseSuccessResponse } from './bonita-bpm-process/bonita-create-case-success-response'
+export { BonitaProcessDefinition } from './bonita-bpm-process/bonita-process-definition'
+
+// Bonita Rest Api Business Data
+export { BonitaBusinessDataService } from './bonita-business-data/bonita-business-data.service'
+export { BonitaBusinessDataContext } from './bonita-business-data/bonita-business-data-context' 
+export { BonitaBusinessDataObject } from './bonita-business-data/bonita-business-data-object'
+export { BonitaBusinessDataQueryParms } from './bonita-business-data/bonita-business-data-query-parms'
+
+// Bonita Rest Api File Upload Component
+export { BonitaFileUploadComponent } from './bonita-file-upload/bonita-file-upload.component'
+export { BonitaFileUploadService } from './bonita-file-upload/bonita-file-upload.service'
+export { BonitaContractInputFile } from './bonita-file-upload/bonita-contract-input-file'
+export { BonitaFileUploadResponse } from './bonita-file-upload/bonita-file-upload-response'
