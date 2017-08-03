be8d892b9e9cde062f2765718dfa6037d8fa6341
Onno Haldar <onno@zgw.nu>
2017-08-03 16:46:25 +0200

    refactor

--- zgwnu-bonita-bpm-process/zgwnu-bonita-process-definition.ts
+++ zgwnu-bonita-bpm-process/zgwnu-bonita-process-definition.ts
@@ -4,23 +4,25 @@ import { ZgwnuBonitaUtils } from '../zgwnu-bonita-rest-api/zgwnu-bonita-utils'
 
 export class ZgwnuBonitaProcessDefinition {
 
-  constructor(processDefinitionData: any) 
+  constructor(processDefinitionData?: any) 
   {
     const utils = new ZgwnuBonitaUtils()
 
-    this.id = processDefinitionData.id
-    this.icon = processDefinitionData.icon
-    this.displayDescription = processDefinitionData.displayDescription
-    this.deploymentDate = utils.getDateValue(processDefinitionData.deploymentDate)
-    this.description = processDefinitionData.description
-    this.activationState = processDefinitionData.activationState
-    this.name = processDefinitionData.name
-    this.deployedBy = processDefinitionData.deployedBy
-    this.displayName = processDefinitionData.displayName
-    this.actorinitiatorid = processDefinitionData.actorinitiatorid
-    this.last_update_date = utils.getDateValue(processDefinitionData.last_update_date)
-    this.configurationState = processDefinitionData.configurationState
-    this.version = processDefinitionData.version
+    if (processDefinitionData) {
+      this.id = processDefinitionData.id
+      this.icon = processDefinitionData.icon
+      this.displayDescription = processDefinitionData.displayDescription
+      this.deploymentDate = utils.getDateValue(processDefinitionData.deploymentDate)
+      this.description = processDefinitionData.description
+      this.activationState = processDefinitionData.activationState
+      this.name = processDefinitionData.name
+      this.deployedBy = processDefinitionData.deployedBy
+      this.displayName = processDefinitionData.displayName
+      this.actorinitiatorid = processDefinitionData.actorinitiatorid
+      this.last_update_date = utils.getDateValue(processDefinitionData.last_update_date)
+      this.configurationState = processDefinitionData.configurationState
+      this.version = processDefinitionData.version
+    }
   }
 
   id: string // "the identifier of the process definition (long)",
